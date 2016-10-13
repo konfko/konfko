ac0a72adc4a0810b2a0710988a063545269737fb
Marko Pipan <marko.pipan@marand.si>
2016-10-13 14:52:48 +0200

    Add simple and override samples

---
+++ konfko-core/src/test/kotlin/com/github/konfko/core/sample/OverridingSettingsSample.kt
@@ -0,0 +1,48 @@
+@file:JvmName("OverridingSettingsSample")
+
+package com.github.konfko.core.sample
+
+import com.github.konfko.core.derived.subSettings
+import com.github.konfko.core.get
+import com.github.konfko.core.source.SettingsMaker
+
+/**
+ * @author markopi
+ */
+fun main(args: Array<String>) {
+    overrideWithSystemProperties()
+    overrideWithSystemPropertiesUnderPrefix()
+}
+
+
+private fun overrideWithSystemProperties() {
+    // override settings in dataSources.properties with any system property
+    System.setProperty("dataSource.first.username", "overrideUsername")
+    val settings = SettingsMaker().make {
+        systemProperties()
+        classpath("com/github/konfko/core/dataSources.properties")
+    }
+
+    val firstUsername: String = settings["dataSource.first.username"]
+    val secondUsername: String = settings["dataSource.second.username"]
+
+    println("First username: $firstUsername, Second username: $secondUsername")
+
+    System.clearProperty("dataSource.first.username")
+}
+
+private fun overrideWithSystemPropertiesUnderPrefix() {
+    // require a namespace for system properties overrides
+    System.setProperty("dataSource.first.username", "ignoredUsername")
+    System.setProperty("konfko.dataSource.first.password", "overridePassword")
+    val settings = SettingsMaker().make {
+        systemProperties() transform { it.subSettings("konfko") }
+        classpath("com/github/konfko/core/dataSources.properties")
+    }
+
+    val firstUsername: String = settings["dataSource.first.username"]
+    val firstPassword: String = settings["dataSource.first.password"]
+    println("First username: $firstUsername, First password: $firstPassword")
+
+    System.clearProperty("konfko.dataSource.first.password")
+}
---
+++ konfko-core/src/test/kotlin/com/github/konfko/core/sample/SimpleSettingsSample.kt
@@ -0,0 +1,35 @@
+@file:JvmName("SimpleSettingsSample")
+
+package com.github.konfko.core.sample
+
+import com.github.konfko.core.derived.subSettings
+import com.github.konfko.core.get
+import com.github.konfko.core.getIfPresent
+import com.github.konfko.core.source.SettingsMaker
+
+/**
+ * @author markopi
+ */
+
+fun main(args: Array<String>) {
+    val settings = SettingsMaker().make {
+        classpath("com/github/konfko/core/dataSources.properties")
+    }
+
+    // read a single setting
+    val firstUrl: String = settings["dataSource.first.url"]
+
+    // partial views
+    val firstDataSource = settings.subSettings("dataSource.first")
+    val secondDataSource = settings.subSettings("dataSource").subSettings("second")
+    val secondUrl: String = secondDataSource["url"]
+
+    println("First url: $firstUrl, second url: $secondUrl")
+
+    // optional Int settings
+    val firstConnectionTimeout: Int? = firstDataSource.getIfPresent("connectionTimeout")
+    val secondConnectionTimeout: Int? = secondDataSource.getIfPresent("connectionTimeout")
+    println("First timeout: $firstConnectionTimeout, second timeout: $secondConnectionTimeout")
+
+    println("All data sources: ${settings.subSettings("dataSource").topLevelKeys}")
+}
